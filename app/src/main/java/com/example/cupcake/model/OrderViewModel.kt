d5357705852ef0d7dd5e3daaaa019faa97e35d17
Gabriel <gabriel99luques@gmail.com>
2022-05-04 17:38:17 -0300

    Update price in view model

--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -6,6 +6,8 @@ import androidx.lifecycle.ViewModel
 import java.text.SimpleDateFormat
 import java.util.*
 
+private const val PRICE_PER_CUPCAKE = 2.00
+
 
 class OrderViewModel : ViewModel() {
 
@@ -30,6 +32,7 @@ class OrderViewModel : ViewModel() {
 
     fun setQuantity(numberCupcakes: Int) {
         _quantity.value = numberCupcakes
+        updatePrice()
     }
 
     fun setFlavor(desiredFlavor: String) {
@@ -63,5 +66,10 @@ class OrderViewModel : ViewModel() {
         _date.value = dateOptions[0]
         _price.value = 0.0
     }
+
+    private fun updatePrice() {
+        _price.value = (quantity.value ?: 0) * PRICE_PER_CUPCAKE
+    }
+
 
 }
